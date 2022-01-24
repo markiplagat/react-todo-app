d06eb49cd70f0959cfb68e3732b99b2df6813980
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-01-24 08:53:42 +0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navbar = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Home',
+    },
+    {
+      id: 2,
+      path: '/about',
+      text: 'About',
+    },
+  ]
+
   return(
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <nav className="navBar">
+      <ul>
+        {links.map(link => {
+          return(
+            <li key={link.id}>
+              <Link
+                to={link.path}
+                activeclassname="active-link"
+                exact
+              >
+                {link.text}
+              </Link>
+            </li>
+            )
+        })}
+      </ul>
+    </nav>
   )
 }
 
