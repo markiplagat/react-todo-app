1d212c083f93c008328dbfc47bafa2a394164383
markiplagat <markkiplagatkipruttoh@gmail.com>
2022-01-24 10:32:24 +0300

    Nested routing

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route exact path="/" element={<TodoContainer />} />
-        <Route path="/about" element={<About /> } />
+        <Route path="/about/*" element={<About /> } />
         <Route path="*" element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Link, Route, Routes} from "react-router-dom";
+import SinglePage from "./SinglePage";
 
 const About = () => {
-  return <div>About Page</div>
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="about-app">About App</Link>
+        </li>
+        <li>
+          <Link to="about-author">About Author</Link>
+        </li>
+          <Routes>
+            <Route path=":slug" element={<SinglePage />}/>
+          </Routes>
+      </ul>
+    </div>
+  );
 }
 
 export default About;
